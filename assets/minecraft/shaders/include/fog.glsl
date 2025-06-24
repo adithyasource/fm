fd097028372edcc773f123d09e8dd27c2297fa64
adithyasource <adithyasrc@gmail.com>
2025-06-25 04:34:12 +0530

    feat: more bearable fog at lower render distance

--- assets/minecraft/shaders/include/fog.glsl
+++ assets/minecraft/shaders/include/fog.glsl
@@ -1,12 +1,5 @@
 #version 150
 
-// ===== fog configuration =====
-#define FOG_START_MULTIPLIER 0.01
-#define FOG_END_MULTIPLIER 1.0
-#define FOG_DENSITY 1.0
-#define FOG_CURVE_POWER 0.4
-// ==============================
-
 layout(std140) uniform Fog {
     vec4 FogColor;
     float FogEnvironmentalStart;
@@ -18,15 +11,17 @@ layout(std140) uniform Fog {
 };
 
 #ifndef PREVENT_NO_FOG_OVERRIDES
-float applyFogCurve(float t) {
-    return pow(t, FOG_CURVE_POWER); // Configurable curve
-}
-
 float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
-    float customFogStart = fogStart * FOG_START_MULTIPLIER;
-    float customFogEnd = fogEnd * FOG_END_MULTIPLIER;
+    float startMultiplier;
+
+    float chunksValue = clamp(fogEnd / 16.0, 2.0, 32.0);
+    startMultiplier = mix(0.2, 0.001, (chunksValue - 16.0) / (32.0 - 2.0));
+
+    float customFogStart = fogStart * startMultiplier;
+    float customFogEnd = fogEnd;
+
     float linearFog = smoothstep(customFogStart, customFogEnd, vertexDistance);
-    return clamp(applyFogCurve(linearFog) * FOG_DENSITY, 0.0, 1.0);
+    return clamp(pow(linearFog, 0.35), 0.0, 1.0);
 }
 #endif
 
