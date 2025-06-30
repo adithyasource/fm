e8d95e4a65dc4e741c8be3b554b0014eff82f5af
adithyasource <adithyasrc@gmail.com>
2025-06-30 17:36:45 +0530

    feat: clean up files for final publishing

--- assets/minecraft/shaders/include/fog.glsl
+++ assets/minecraft/shaders/include/fog.glsl
@@ -6,34 +6,11 @@ layout(std140) uniform Fog {
     float FogEnvironmentalEnd;
     float FogRenderDistanceStart;
     float FogRenderDistanceEnd;
-    float FogSkyEnd;
-    float FogCloudsEnd;
+    float FogSkyEnd; // unused
+    float FogCloudsEnd; // unused
 };
 
-#ifndef PREVENT_NO_FOG_OVERRIDES
-float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
-    float startMultiplier;
-
-    float chunksValue = clamp(fogEnd / 16.0, 2.0, 32.0);
-    startMultiplier = mix(0.2, 0.001, (chunksValue - 16.0) / (32.0 - 2.0));
-
-    float customFogStart = fogStart * startMultiplier;
-    float customFogEnd = fogEnd;
-
-    float linearFog = smoothstep(customFogStart, customFogEnd, vertexDistance);
-    return clamp(pow(linearFog, 0.35), 0.0, 1.0);
-}
-#endif
-
-float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd) {
-    return max(linear_fog_value(sphericalVertexDistance, environmentalStart, environmantalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
-}
-
-vec4 apply_fog(vec4 inColor, float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd, vec4 fogColor) {
-    float fogValue = total_fog_value(sphericalVertexDistance, cylindricalVertexDistance, environmentalStart, environmantalEnd, renderDistanceStart, renderDistanceEnd);
-    return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
-}
-
+// helper functions for fog distance
 float fog_spherical_distance(vec3 pos) {
     return length(pos);
 }
@@ -42,4 +19,25 @@ float fog_cylindrical_distance(vec3 pos) {
     float distXZ = length(pos.xz);
     float distY = abs(pos.y);
     return max(distXZ, distY);
+}
+
+// overwriting default linear fog
+float linear_fog_value(float vertexDistance, float fogStart, float fogEnd) {
+    float chunksValue = clamp(fogEnd / 16.0, 2.0, 32.0);
+    float startMultiplier = mix(0.2, 0.001, (chunksValue - 16.0) / (32.0 - 2.0));
+    float customFogStart = fogStart * startMultiplier;
+    float linearFog = smoothstep(customFogStart, fogEnd, vertexDistance);
+    return clamp(pow(linearFog, 0.35), 0.0, 1.0);
+}
+
+// combining both cylindrical and spherical fog in the final render
+float total_fog_value(float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd) {
+    return max(linear_fog_value(sphericalVertexDistance, environmentalStart, environmantalEnd), linear_fog_value(cylindricalVertexDistance, renderDistanceStart, renderDistanceEnd));
+}
+
+// final function to apply fog
+// also used in sky.fsh to add fog to sky
+vec4 apply_fog(vec4 inColor, float sphericalVertexDistance, float cylindricalVertexDistance, float environmentalStart, float environmantalEnd, float renderDistanceStart, float renderDistanceEnd, vec4 fogColor) {
+    float fogValue = total_fog_value(sphericalVertexDistance, cylindricalVertexDistance, environmentalStart, environmantalEnd, renderDistanceStart, renderDistanceEnd);
+    return vec4(mix(inColor.rgb, fogColor.rgb, fogValue * fogColor.a), inColor.a);
 }
